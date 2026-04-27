506555453152c46988c7adcaa1bd3b9028fa2923
JeDiFoXxX <serjik1993991@gmail.com>
2026-04-27 16:12:33 +0300

    Генератор HTML

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String getIndex() {
         return "index";
     }
+
+    @GetMapping("/hello")
+    public String getHelloWorld() {
+        return "hello";
+    }
 }
